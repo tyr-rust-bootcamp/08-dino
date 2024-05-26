345da4837511c06d2b8c3328e01c81c82d92b719
Tyr Chen <tyr.chen@gmail.com>
2024-05-26 16:01:00 -0700

    feature: default to IIFE for ModuleType

--- bundler/src/lib.rs
+++ bundler/src/lib.rs
@@ -20,7 +20,7 @@ mod tests {
     #[test]
     fn bundle_ts_should_work() -> Result<()> {
         let ret = run_bundle("fixtures/main.ts", &Default::default())?;
-        assert_eq!(ret, "async function execute(name){console.log(\"Executing lib\");return`Hello ${name}!`;}async function main(){console.log(\"Executing main\");console.log(await execute(\"world\"));}export{main as default};");
+        assert_eq!(ret, "(function(){async function execute(name){console.log(\"Executing lib\");return`Hello ${name}!`;}async function main(){console.log(\"Executing main\");console.log(await execute(\"world\"));}return{default:main};})();");
         Ok(())
     }
 }
